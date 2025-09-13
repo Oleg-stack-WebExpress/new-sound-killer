603608e8afd41c18f809e05a63bb213ac4c49548
Your Name <you@example.com>
2025-09-13 07:08:43 +0700

    разработка footer

--- footer.php
+++ footer.php
@@ -1,7 +1,34 @@
 </main>
 
 <footer class="footer">
-    
+    <div class="container">
+        <!-- Первая строка -->
+        <div class="footer__head">
+            <div class="footer__head__text">
+                <h3>Ready to get started?</h3>
+                <p>Create your free account now</p>
+            </div>
+            <div class="footer__head__action">
+                <div class="buttons">
+                    <a class="btn btn-primary">Try it free</a>
+                    <a class="btn btn-outline-primary">Chat with us</a>
+                </div>
+            </div>
+        </div>
+
+        <!-- Вторая строка -->
+        <div class="columns footer__body">
+            1
+        </div>
+        <!-- Третья строка -->
+        <div class="footer__copyright">
+            <p>
+                © 2019-2020 | <a href="https://cssninja.io/">Css Ninja</a> | All
+                Rights Reserved.
+            </p>
+
+        </div>
+    </div>
 </footer>
 
 <?php wp_footer(); ?>
--- page-index.php
+++ page-index.php
@@ -2,6 +2,12 @@
 get_header();
 ?>
 
+<a class="btn btn-primary">Buy Now</a>
+<a class="btn btn-outline-primary">Features</a>
+
+<h1 class="title title--main">Meet the New Sound Killer.</h1>
+<h2 class="title">Headphones of the future</h2>
+<h2 class="title title--center">Headphones of the future</h2>
 
 
 <?php
