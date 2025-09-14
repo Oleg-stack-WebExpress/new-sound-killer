44c7c1ea9c22b4824e7228e041cdf34e13f12e18
Your Name <you@example.com>
2025-09-15 03:24:45 +0700

    footer finish

--- footer.php
+++ footer.php
@@ -2,7 +2,6 @@
 
 <footer class="footer">
     <div class="container">
-        <!-- Первая строка -->
         <div class="footer__head">
             <div class="footer__head__text">
                 <h3>Ready to get started?</h3>
@@ -15,12 +14,98 @@
                 </div>
             </div>
         </div>
-
-        <!-- Вторая строка -->
         <div class="columns footer__body">
-            1
+            <div class="row">
+                <div class="col-6 columns-description">
+                    <img class="small-footer-logo" src="<?= assets('images/bulkit-logo-g.png'); ?>" alt="logo">
+                    <div class="footer-description">
+                        <p>Bulkit is built for developers and designers. It's modular approach
+                            lets you create an original landing page for your brand.</p>
+                    </div>
+                    <span class="moto">
+                        <p>Designed and coded with <i class="icon-heart"></i>
+                            by CSS Ninja.
+                        </p>
+                    </span>
+                    <div class="social-links">
+                        <a href="#">
+                            <span class="icon">
+                                <i class="icon-facebook"></i>
+                            </span>
+                        </a>
+                        <a href="#">
+                            <span class="icon">
+                                <i class="icon-twitter"></i>
+                            </span>
+                        </a>
+                        <a href="#">
+                            <span class="icon">
+                                <i class="icon-linkedin"></i>
+                            </span>
+                        </a>
+                        <a href="#">
+                            <span class="icon">
+                                <i class="icon-dribbble"></i>
+                            </span>
+                        </a>
+                        <a href="#">
+                            <span class="icon">
+                                <i class="icon-github-circled"></i>
+                            </span>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-2 pages-main margin-left-195">
+                    <ul class="footer__column">
+                        <li class="column-header">BULKIT</li>
+                        <li class="column-item">
+                            <a href="#">Home</a>
+                        </li>
+                        <li class="column-item">
+                            <a href="#">Pricing</a>
+                        </li>
+                        <li class="column-item">
+                            <a href="#">Get started</a>
+                        </li>
+                        <li class="column-item">
+                            <a href="#">Help</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-2 pages-additionally">
+                    <ul class="footer__column">
+                        <li class="column-header">RESSOURCES</li>
+                        <li class="column-item">
+                            <a href="#">Learning</a>
+                        </li>
+                        <li class="column-item">
+                            <a href="#">Support center</a>
+                        </li>
+                        <li class="column-item">
+                            <a href="#">Frequent questions</a>
+                        </li>
+                        <li class="column-item">
+                            <a href="#">Schedule a demo</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-2 pages-terms">
+                    <ul class="footer__column">
+                        <li class="column-header">TERMS</li>
+                        <li class="column-item">
+                            <a href="#">Terms of Service</a>
+                        </li>
+                        <li class="column-item">
+                            <a href="#">Privacy policy</a>
+                        </li>
+                        <li class="column-item">
+                            <a href="#">SaaS services</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        <!-- Третья строка -->
+
         <div class="footer__copyright">
             <p>
                 © 2019-2020 | <a href="https://cssninja.io/">Css Ninja</a> | All
