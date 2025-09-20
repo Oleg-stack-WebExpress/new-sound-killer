57f30b91b88096a3532d29840a4b87878d3e8890
Your Name <you@example.com>
2025-09-20 08:29:31 +0700

    Работа над лого

--- header.php
+++ header.php
@@ -6,44 +6,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo('name'); ?></title>
 
-    <!-- Вебмастер -->
-
-    <!-- /Вебмастер  -->
-
-
     <?php wp_head(); ?>
-
-    <!-- Yandex.Metrika counter -->
-
-    <!-- /Yandex.Metrika counter -->
 
 </head>
 
 <body>
     <header class="header">
-        <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <div class="header__first">
+                <img class="logo" src="<?= assets('images/bulkit-core.png'); ?>" alt="logo"></img><span
+                    class="logo__left">BUL<span class="logo__rigth">KIT</span></span>
+            </div>
+
+
+        </div>
+    </header>
+
+
+
+    <!-- <nav class="navbar navbar-expand-lg">
             <div class="container">
                 <div class="header__grid">
-                    <!-- Меню -->
+                    Меню 
                     <div class="header__col header__col--menu">
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <?php
-                            wp_nav_menu([
-                                'theme_location' => 'top',
-                                'depth' => 2,
-                                'container' => false,
-                                'menu_class' => 'navbar-nav',
-                                'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
-                                'walker' => new WP_Bootstrap_Navwalker(),
-                                'link_class' => 'nav-link'
-                            ]);
+                            //wp_nav_menu([
+                            //  'theme_location' => 'top',
+                            //  'depth' => 2,
+                            //    'container' => false,
+                            //    'menu_class' => 'navbar-nav',
+                            //    'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
+                            //    'walker' => new WP_Bootstrap_Navwalker(),
+                            //    'link_class' => 'nav-link'
+                            //   ]);
                             ?>
                         </div>
-                    </div>
+                    </div>-->
 
-                    <!--наполнение меню еще...-->
+    <!--наполнение меню еще...-->
 
-                    <!-- Кнопка меню -->
+    <!-- Кнопка меню 
                     <div class="header__col header__col--toggler">
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
@@ -53,7 +56,7 @@
                     </div>
                 </div>
             </div>
-        </nav>
-    </header>
+        </nav>-->
+
 
     <main class="main-content">
