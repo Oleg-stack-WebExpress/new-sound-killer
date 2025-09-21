955e1ef30bfecf3851cad7edbef36174a28d2188
Your Name <you@example.com>
2025-09-21 08:20:27 +0700

    Верстка хедера

--- header.php
+++ header.php
@@ -12,51 +12,34 @@
 
 <body>
     <header class="header">
-        <div class="container">
-            <div class="header__first">
-                <img class="logo" src="<?= assets('images/bulkit-core.png'); ?>" alt="logo"></img><span
-                    class="logo__left">BUL<span class="logo__rigth">KIT</span></span>
-            </div>
-
-
-        </div>
-    </header>
-
-
-
-    <!-- <nav class="navbar navbar-expand-lg">
+        <nav class="navbar navbar-expand-lg">
             <div class="container">
-                <div class="header__grid">
-                    Меню 
-                    <div class="header__col header__col--menu">
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                            <?php
-                            //wp_nav_menu([
-                            //  'theme_location' => 'top',
-                            //  'depth' => 2,
-                            //    'container' => false,
-                            //    'menu_class' => 'navbar-nav',
-                            //    'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
-                            //    'walker' => new WP_Bootstrap_Navwalker(),
-                            //    'link_class' => 'nav-link'
-                            //   ]);
-                            ?>
-                        </div>
-                    </div>-->
-
-    <!--наполнение меню еще...-->
-
-    <!-- Кнопка меню 
-                    <div class="header__col header__col--toggler">
-                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                    </div>
+                <div class="header__first">
+                    <img class="logo" src="<?= assets('images/bulkit-core.png'); ?>" alt="logo"></img><span
+                        class="logo__left">BUL<span class="logo__rigth">KIT</span></span>
                 </div>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <?php
+                    wp_nav_menu([
+                        'theme_location' => 'top',
+                        'depth' => 2,
+                        'container' => false,
+                        'menu_class' => 'navbar-nav',
+                        'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
+                        'walker' => new WP_Bootstrap_Navwalker(),
+                        'link_class' => 'nav-link'
+                    ]);
+                    ?>
+                </div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a href="#"><i class="icon-heart-empty"></i></a>
+                <a href="#"><i class="icon-basket-alt"></i></a>
+                <img class="avatar" src="<?= assets('images/helen.jpg'); ?>" alt="avatar">
             </div>
-        </nav>-->
-
-
+        </nav>
+    </header>
     <main class="main-content">
