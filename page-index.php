9ad0258d9a62527529e733e33dbb96aa6e0bc8a2
Your Name <you@example.com>
2025-09-28 07:49:31 +0700

    Работа с секцией name

--- page-index.php
+++ page-index.php
@@ -4,10 +4,10 @@ get_header();
 
 <section class="meet">
     <div class="container">
-        <div class="row">
-            <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6 "><img
+        <div class="row align-items-center">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6 text-center"><img
                     src="<?= assets('images/headphones-float.png'); ?>" alt="sound"></div>
-            <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6 page-column">
+            <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-6 page-column text-center text-xl-start">
                 <h1 class="title title__main">Meet the New Sound Killer.</h1>
                 <p>
                     Beats' first on-ear headphones with active noise cancellation. The
@@ -24,9 +24,36 @@ get_header();
     </div>
 </section>
 
+<section class="logos padding-top-50">
+    <div class="container">
+        <div class="logos__action">
+            <a href="#"><img src="<?= assets('images/covenant.svg'); ?>" alt="covenant"></img></a>
+            <a href="#"><img href="#" src="<?= assets('images/infinite.svg'); ?>" alt="infinite"></img></a>
+            <a href="#"><img href="#" src="<?= assets('images/phasekit.svg'); ?>" alt="phasekit"></img></a>
+            <a href="#"><img href="#" src="<?= assets('images/grubspot.svg'); ?>" alt="grubspot"></img></a>
+            <a href="#"><img href="#" src="<?= assets('images/gutwork.svg'); ?>" alt="gutwork"></img></a>
+        </div>
+    </div>
+</section>
+
 <section class="name padding-top-100">
     <div class="container">
+        <div class="row align-items-center">
+    <div class="col-12 col-xl-6 page-column text-center text-xl-start">
         <h2 class="title">Headphones of the future</h2>
+        <p class="mx-auto mx-xl-0" style="max-width: 500px;">
+            Lorem ipsum dolor sit amet, clita laoreet ne cum. His cu harum
+            inermis iudicabit. Ex vidit fierent hendrerit eum, sed stet
+            periculis ut.
+        </p>
+        <a href="#" class="btn btn-outline-primary d-inline-block">
+            Learn more <i class="icon-right-open-mini"></i>
+        </a>
+    </div>
+    <div class="col-12 col-xl-6 text-center padding-top-50">
+        <img src="<?= assets('images/imgbb.com.png'); ?>" alt="imgbb.com" class="img-fluid">
+    </div>
+</div>
     </div>
 </section>
 
